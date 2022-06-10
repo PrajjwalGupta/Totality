123e7efe5057a79a67e1cb173f9fb7a4f027abc3
Prajjwal Gupta <31537066+PrajjwalGupta@users.noreply.github.com>
2022-06-10 20:18:38 +0530

    Initial Commit

---
+++ Totality/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Totality
+//
+//  Created by Prajjawal Gupta on 10/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Totality/TotalityApp.swift
@@ -0,0 +1,17 @@
+//
+//  TotalityApp.swift
+//  Totality
+//
+//  Created by Prajjawal Gupta on 10/06/22.
+//
+
+import SwiftUI
+
+@main
+struct TotalityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
